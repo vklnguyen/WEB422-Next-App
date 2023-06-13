28f90d08e4aeb5dcfe5d5166bfe5d084f0ddea2b
Vu Khanh Linh Nguyen <vklnguyen@myseneca.ca>
2023-06-13 17:20:50 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB422 – Assignment 3
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: __Vu Khanh Linh Nguyen___ Student ID: __125710210__ Date: __June 13, 2023___
+*
+*
+********************************************************************************/ 
+
+
 import useSWR from 'swr';
 import { useState, useEffect } from 'react';
 import { Pagination, Accordion } from 'react-bootstrap';
