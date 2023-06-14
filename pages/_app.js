c377ebf760a54774fd18114747be18522f00470e
Vu Khanh Linh Nguyen <vklnguyen@myseneca.ca>
2023-06-13 22:16:47 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from '@/components/Layout';
+import Layout from '@/components/LayOut';
 import { SWRConfig } from 'swr';
 
 export default function App({ Component, pageProps }) {
